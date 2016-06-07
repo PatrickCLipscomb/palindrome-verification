a1ca0e22aeb6442bfee24a2815b965ead567c405
Cameron Poore and Patrick Lipscomb <cameron@>
2016-06-07 11:41:32 -0700

    create a page that will tell you if your one word input is a palindrome.

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $('form#blanks').submit(function(event) {
+    event.preventDefault();
+    var userInput = $('input#pali').val();
+    var output = [];
+    for ( var index = 0; index < userInput.length; index += 1) {
+      output.push(userInput[index]);
+    }
+    var putout = output.slice();
+    var putout = putout.reverse();
+    var answernew = putout.join(' ');
+    var answerold = output.join(' ');
+
+    if ( answernew === answerold ) {
+      $('.answertrue').text("This is a palindrome");
+      $('.pali').text(answernew);
+    } else if ( answernew !== answerold) {
+      $('.answerfalse').text('Unfortunately, this is jibberish');
+      $('.pali').text(answernew);
+    } else {
+      console.log();
+    }
+    $('#blanks').hide();
+  });
+});
